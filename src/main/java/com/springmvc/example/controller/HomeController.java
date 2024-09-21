98e39a765b52cae1812e239506d7c2e19620cf08
Devanshu Kushwah <devanshukushwah.001@gmail.com>
2024-09-21 18:14:21 +0530

    change ModelAndView to Model example.

--- src/main/java/com/springmvc/example/controller/HomeController.java
+++ src/main/java/com/springmvc/example/controller/HomeController.java
@@ -1,9 +1,9 @@
 package com.springmvc.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -14,11 +14,9 @@ public class HomeController {
     }
 
     @RequestMapping("/add")
-    ModelAndView add(@RequestParam int num1, @RequestParam int num2) {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("result");
+    String add(@RequestParam int num1, @RequestParam int num2, Model m) {
         int num3 = num1 + num2;
-        modelAndView.addObject("num3", num3);
-        return modelAndView;
+        m.addAttribute("num3", num3);
+        return "result";
     }
 }
